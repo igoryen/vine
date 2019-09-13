67bac058bacad4de78b94e57602d2339c78339c1
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-09-13 15:16:44 -0400

    added a submit item section/component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="app">
         <Shapka />
+        <ItemAppender />
         <Points v-bind:list="list" v-on:del-item="scrapItem"/>
     </div>
 </template>
@@ -9,12 +10,15 @@
 
 import Shapka from './components/layout/Header';
 import Points from './components/Todos';
+import ItemAppender from './components/AddTodoItem';
+
 
 export default {
     name: 'app',
     components: {
         Shapka,
-        Points
+        Points,
+        ItemAppender
     },
     data() {
         return {
@@ -48,5 +52,17 @@ export default {
 <style>
     body {
         font-family: Georgia, 'Times New Roman', Times, serif;
+    }
+    .btn {
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+        font-family: Georgia, 'Times New Roman', Times, serif;
+    }
+    .btn:hover {
+        background: #666;
     }
 </style>
---
+++ src/components/AddTodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <form>
+            <input type="text" name="title" placeholder="Add a Todo item...">
+            <input type="submit" value="Hand In" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppendItem"
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+    input[type="text"] {
+        flex: 10px;
+        padding: 5px;
+    }
+    input[type="submit"] {
+        flex: 2;
+    }
+</style>
