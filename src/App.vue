820541148d805b511d0b34fd3fdff181cc384ac8
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-09-16 10:19:31 -0400

    installed vue-router through the Vue UI

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,12 @@
 <template>
-    <div id="app">
-        <Shapka />
-        <ItemAppender v-on:add-item="aggregateItem"/>
-        <Points v-bind:list="list" v-on:del-item="scrapItem"/>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-
-import Shapka from './components/layout/Header';
-import Points from './components/Todos';
-import ItemAppender from './components/AddTodoItem';
-import axios from 'axios'; // #3
-
-
-export default {
-    name: 'app',
-    components: {
-        Shapka,
-        Points,
-        ItemAppender
-    },
-    data() {
-        return {
-            list: []
-        }
-    },
-    methods: {
-        scrapItem( id ) {
-            axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`) // #14
-            .then( response => this.list = this.list.filter( item => item.id !== id)) // #15
-            .catch(err => console.log(err));// #6
-        },
-        aggregateItem( brandNewItem ) {
-            const { title, completed } = brandNewItem;// #8
-            axios.post('https://jsonplaceholder.typicode.com/todos', {// #13
-                title, // #10
-                completed // #11
-            })// #9
-            .then( response => this.list = [...this.list, response.data]) // #12
-            .catch(err => console.log(err));// #6
-            
-        }
-    }, 
-    created() { // #2
-        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10') // #4
-        .then( response => this.list = response.data) // #5
-        .catch(err => console.log(err));// #6
-    }
-}
-</script>
 
 <style>
     body {
