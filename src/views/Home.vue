ae868c65037e8a68aba6997664983d210b2a8077
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-09-16 10:33:15 -0400

    fixed the error on the previous commit when I pasted the contents into the wrong file.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Shapka />
+        <ItemAppender v-on:add-item="aggregateItem"/>
+        <Points v-bind:list="list" v-on:del-item="scrapItem"/>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Shapka from '../components/layout/Header';
+import Points from '../components/Todos';
+import ItemAppender from '../components/AddTodoItem';
+import axios from 'axios'; // #3
+
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    components: {
+        Shapka,
+        Points,
+        ItemAppender
+    },
+    data() {
+        return {
+            list: []
+        }
+    },
+    methods: {
+        scrapItem( id ) {
+            axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`) // #14
+            .then( response => this.list = this.list.filter( item => item.id !== id)) // #15
+            .catch(err => console.log(err));// #6
+        },
+        aggregateItem( brandNewItem ) {
+            const { title, completed } = brandNewItem;// #8
+            axios.post('https://jsonplaceholder.typicode.com/todos', {// #13
+                title, // #10
+                completed // #11
+            })// #9
+            .then( response => this.list = [...this.list, response.data]) // #12
+            .catch(err => console.log(err));// #6
+            
+        }
+    }, 
+    created() { // #2
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10') // #4
+        .then( response => this.list = response.data) // #5
+        .catch(err => console.log(err));// #6
+    }
 }
 </script>
+
+<style>
+    body {
+        font-family: Georgia, 'Times New Roman', Times, serif;
+    }
+    .btn {
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+        font-family: Georgia, 'Times New Roman', Times, serif;
+    }
+    .btn:hover {
+        background: #666;
+    }
+</style>
