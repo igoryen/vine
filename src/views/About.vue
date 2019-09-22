b00117553e0d03ab38177cf69895fa6049f5dcef
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-09-22 19:56:10 -0400

    Maximilian Schwarzmüller: lecture 3

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,25 @@
   <div class="about">
     <h1>About</h1>
     <p>This is the TodoList app v1.0.0. It is part of the Vue crash course on Youtube.</p>
+    <div id="pickle">
+      <input type="text" v-on:input="changePhrase"> 
+      <p>{{something}}</p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+    name: 'Pickle',
+    data(){
+        return {
+            something: "'Tis I, a pickle!"
+        }        
+    },
+    methods: {
+        changePhrase: function( event ){
+            this.something = event.target.value;
+        }
+    }
+}
+</script>
