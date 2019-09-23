49e6a0732798598c3fad4e34dfe6ffda92374e1f
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-09-22 20:44:58 -0400

    Maximilian Schwarzmüller: block 11. Getting value from 'data'

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,10 +3,13 @@
     <h1>About</h1>
     <p>This is the TodoList app v1.0.0. It is part of the Vue crash course on Youtube.</p>
     <div id="pickle">
-      <input type="text" v-on:input="changePhrase"> 
-      <p>{{something}}</p>
-      <div>
+      <!-- <input type="text" v-on:input="changePhrase">  -->
+      <!-- <p>{{ something }}</p> -->
+      <div class="greeting english">
           <h1>{{ sayHello() }}</h1>
+      </div>
+      <div class="greeting spanish">
+          <h1>{{ dimeHola() }}</h1>
       </div>
     </div>
   </div>
@@ -15,19 +18,25 @@
 <script>
 export default {
     name: 'Pickle',
-    data(){
+    data: function(){
         return {
-            something: "'Tis I, a pickle!"
+            // things: [
+            //     { something: "'Tis I, a pickle!"},
+            //     { spanish: "Hola!" }
+            // ] 
+            spanish: "Hola!!!"           
         }        
     },
     methods: {
-        changePhrase: function( event ){
-            this.something = event.target.value;
-        },
+        // changePhrase: function( event ){
+        //     this.something = event.target.value;
+        // },
         sayHello: function(){
             return "Hello!";
-        }
-        
+        },
+        dimeHola: function(){
+            return this.spanish;
+        }        
     }
 }
 </script>
